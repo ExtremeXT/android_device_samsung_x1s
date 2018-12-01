8091bab45369321d53e6b82c150b48dc9fdb5493
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    x1s: Declare EUICC permissions
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device.mk
+++ device.mk
@@ -38,6 +38,11 @@ PRODUCT_PACKAGES += \
 # Overlays 
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-com.samsung.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.samsung.euicc.xml
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
