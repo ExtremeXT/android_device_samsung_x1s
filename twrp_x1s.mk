d6191787eab52f5751ee38d36de8c798ee35b85d
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-02-25 20:56:15 +0200

    mk: exynos990

--- twrp_x1s.mk
+++ twrp_x1s.mk
@@ -8,7 +8,7 @@
 # Only the below variable(s) need to be changed!
 #
 # Define hardware platform
-PRODUCT_PLATFORM := erd8835
+PRODUCT_PLATFORM := exynos990
 
 # The below variables will be generated automatically
 #
