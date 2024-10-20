2ecae9fec9664d6f75fea9f396a573f6ec7bb57e
Michael Bestas <mkbestas@lineageos.org>
2024-10-20 23:09:47 +0300

    x1s: Adjust override prop key names
    Change-Id: Iaa96fd777196fe90f408457a580b80d86b041140
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- lineage_x1s.mk
+++ lineage_x1s.mk
@@ -36,10 +36,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=x1sxxx \
-    PRIVATE_BUILD_DESC="x1sxxx-user 11 RP1A.200720.012 G981BXXSADUH5 release-keys"
-
-BUILD_FINGERPRINT := samsung/x1sxxx/x1s:11/RP1A.200720.012/G981BXXSADUH5:user/release-keys
+    DeviceProduct=x1sxxx \
+    SystemName=x1sxxx \
+    BuildFingerprint=samsung/x1sxxx/x1s:11/RP1A.200720.012/G981BXXSADUH5:user/release-keys \
+    BuildDesc="x1sxxx-user 11 RP1A.200720.012 G981BXXSADUH5 release-keys"
 
 # Define PDA property for camera
 PRODUCT_SYSTEM_PROPERTIES += \
