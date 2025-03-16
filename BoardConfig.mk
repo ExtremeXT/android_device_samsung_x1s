400127446360614e1eca8ad3eaa95631103bd325
Tim Zimmermann <tim@linux4.de>
2025-03-16 06:57:35 +0100

    x1s: Convert bcmdhd SAE offload flag to soong
    Change-Id: Ia1b0b08f4e21d2c98321dd04986bb814e3d0cb5a
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,6 @@ TARGET_OTA_ASSERT_DEVICE := x1s
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Wi-Fi
-BOARD_WLAN_BCMDHD_SAE                         := true
 BOARD_WLAN_DEVICE                             := bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER                   := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB              := lib_driver_cmd_bcmdhd
@@ -60,6 +59,7 @@ WIFI_HIDL_FEATURE_AWARE                       := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE              := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION                        := VER_0_8_X
+$(call soong_config_set,wpa_supplicant_8,board_wlan_bcmdhd_sae,true)
 
 # inherit from the proprietary version
 -include vendor/samsung/x1s/BoardConfigVendor.mk
